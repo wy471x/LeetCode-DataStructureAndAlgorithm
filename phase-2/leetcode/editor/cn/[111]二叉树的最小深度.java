1275fc6d0066a71d94d33d5e7aec17b13ffafcde
dunk <wy471x@qq.com>
2022-02-24 09:32:57 +0800

    day 14 finished.

---
+++ phase-2/leetcode/editor/cn/[111]二叉树的最小深度.java
@@ -0,0 +1,83 @@
+//给定一个二叉树，找出其最小深度。 
+//
+// 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。 
+//
+// 说明：叶子节点是指没有子节点的节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [2,null,3,null,4,null,5,null,6]
+//输出：5
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数的范围在 [0, 10⁵] 内 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 676 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.LinkedList;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 1;
+
+        while (!queue.isEmpty()) {
+            int sz = queue.size();
+
+            for (int i = 0; i < sz; i++) {
+                TreeNode node = queue.poll();
+
+                // 遇到的第一个左右子树为空的节点即为离根节点最近的叶子节点
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
